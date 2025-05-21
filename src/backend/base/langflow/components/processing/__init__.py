1ef379f2e3cf76fb60157580cfa7bf17ee869d1b
Sebastiaan <751205+SharkWipf@users.noreply.github.com>
2025-05-21 23:57:03 +0200

    Add FormatDirectoryData component

--- src/backend/base/langflow/components/processing/__init__.py
+++ src/backend/base/langflow/components/processing/__init__.py
@@ -12,6 +12,7 @@ from .message_to_data import MessageToDataComponent
 from .parse_data import ParseDataComponent
 from .parse_json_data import ParseJSONDataComponent
 from .parser import ParserComponent
+from .format_directory_data import FormatDirectoryDataComponent
 from .regex import RegexExtractorComponent
 from .select_data import SelectDataComponent
 from .split_text import SplitTextComponent
@@ -33,6 +34,7 @@ __all__ = [
     "ParseDataFrameComponent",
     "ParseJSONDataComponent",
     "ParserComponent",
+    "FormatDirectoryDataComponent",
     "RegexExtractorComponent",
     "SelectDataComponent",
     "SplitTextComponent",
