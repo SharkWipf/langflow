3bae2adfce6fdcf1c605990d75f638f4f81066d9
Sebastiaan <751205+SharkWipf@users.noreply.github.com>
2025-05-22 12:46:38 +0200

    Revert "Fix list input connectors"

--- src/frontend/src/components/core/parameterRenderComponent/types.ts
+++ src/frontend/src/components/core/parameterRenderComponent/types.ts
@@ -7,7 +7,6 @@ import {
 } from "@/types/api";
 import { RangeSpecType } from "@/types/components";
 import { ColumnField } from "@/types/utils/functions";
-import { targetHandleType } from "@/types/flow";
 
 // Base type for RefreshParameterComponent children
 export type BaseInputProps<valueType = any> = {
@@ -19,7 +18,6 @@ export type BaseInputProps<valueType = any> = {
   nodeClass?: APIClassType;
   helperText?: string;
   handleNodeClass?: (value: any, code?: string, type?: string) => void;
-  inputId?: targetHandleType;
   readonly?: boolean;
   placeholder?: string;
   isToolMode?: boolean;
